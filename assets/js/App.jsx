22517fb3f7bf7638bcb59904ed872d2fbfe2e584
Felix <zen9.felix@gmail.com>
2021-03-20 00:35:03 +0800

    Basic routing done

--- assets/js/App.jsx
+++ assets/js/App.jsx
@@ -1,34 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import {
+  BrowserRouter,
+  Route, Switch
+} from "react-router-dom";
+import Home from "./pages/home";
+
+
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

339bbe88029512e6f551825288d064bf14d1f630
Felix <zen9.felix@gmail.com>
2021-06-28 21:02:08 +0800

    Update deps

--- mix.exs
+++ mix.exs
@@ -34,26 +34,23 @@ defmodule Donation.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
+      {:phoenix, "~> 1.5.9"},
       {:phoenix_ecto, "~> 4.1"},
-      # {:ecto_sql, "~> 3.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      # {:phoenix_html, "~> 2.11"},
-      {:phoenix_html, "~> 2.14"},
+      {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_swagger, "~> 0.8"},
       # optional
+      {:phoenix_swagger, "~> 0.8"},
       {:ex_json_schema, "~> 0.5"},
-      {:comeonin, "~> 5.0"},
       {:bcrypt_elixir, "~> 2.0"},
       {:timex, "~> 3.7"},
-      {:faker, "~> 0.16", only: :test},
-      ## PDF OUTPUT
       {:pdf_generator, "~> 0.6.2"}
     ]
   end

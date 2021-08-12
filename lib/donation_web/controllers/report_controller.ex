7cbadf0907e81047161e145dfc54a1cb7f5e917f
jclin8 <jclin726@gmail.com>
2021-08-12 10:16:28 +0800

    Filter out FAILED in report, Display chinese in PDF, Mass Offering PDF

--- lib/donation_web/controllers/report_controller.ex
+++ lib/donation_web/controllers/report_controller.ex
@@ -13,18 +13,21 @@ defmodule DonationWeb.ReportController do
   ## LIST DONATIONS -> CASE 1
   def list_donations(conn, %{"search" => search}) do
     query = Admins.filter_donations(%{search: search})
+            |> Admins.reject_verified_is_false()
     render(conn, :list_donations, donations: query)
   end
 
   ## LIST DONATIONS -> CASE 2
   def list_donations(conn, _) do
     query = Admins.filter_donations(%{search: nil})
+            |> Admins.reject_verified_is_false()
     render(conn, :list_donations, donations: query)
   end
 
   ## LIST DONATIONS -> EXPORT EXCEL
   def list_donations_xlsx(conn, %{"search" => search}) do
     query = Admins.filter_donations(%{search: search})
+            |> Admins.reject_verified_is_false()
     conn
     |> put_resp_content_type("text/xlsx")
     |> put_resp_header("content-disposition", "attachment; filename=donations.xlsx;")
@@ -35,6 +38,7 @@ defmodule DonationWeb.ReportController do
   def list_mass_offerings(conn, %{"search" => search}) do
     new_date = Date.from_iso8601!(search["from_date"])
     query = Admins.find_mass_offering_dates(new_date)
+            |> Admins.reject_verified_is_false()
     render(conn, :list_mass_offerings, mass_offerings: query)
   end
 
@@ -42,6 +46,7 @@ defmodule DonationWeb.ReportController do
   def list_mass_offerings(conn, _) do
     default_date = Date.utc_today()
     query = Admins.find_mass_offering_dates(default_date)
+            |> Admins.reject_verified_is_false()
     render(conn, :list_mass_offerings, mass_offerings: query)
   end
 
@@ -50,18 +55,16 @@ defmodule DonationWeb.ReportController do
     from_date = get_in(params, ["search", "from_date"])
     new_date = Date.from_iso8601!(from_date)
     query = Admins.find_mass_offering_dates(new_date)
-
+            |> Admins.reject_verified_is_false()
     html =
       Phoenix.View.render_to_string(DonationWeb.ReportView, "list_mass_offerings_pdf.html",
         layout: {DonationWeb.LayoutView, "pdf.html"},
         conn: conn,
         mass_offerings_eng_tg: Admins.filter_mass_intentions(query, "English", "Thanksgiving"),
-        mass_offerings_eng_si:
-          Admins.filter_mass_intentions(query, "English", "Special Intention"),
+        mass_offerings_eng_si: Admins.filter_mass_intentions(query, "English", "Special Intention"),
         mass_offerings_eng_ds: Admins.filter_mass_intentions(query, "English", "Departed Soul"),
         mass_offerings_chi_tg: Admins.filter_mass_intentions(query, "Mandarin", "Thanksgiving"),
-        mass_offerings_chi_si:
-          Admins.filter_mass_intentions(query, "Mandarin", "Special Intention"),
+        mass_offerings_chi_si: Admins.filter_mass_intentions(query, "Mandarin", "Special Intention"),
         mass_offerings_chi_ds: Admins.filter_mass_intentions(query, "Mandarin", "Departed Soul"),
         mass_offerings_tml_tg: Admins.filter_mass_intentions(query, "Tamil", "Thanksgiving"),
         mass_offerings_tml_si: Admins.filter_mass_intentions(query, "Tamil", "Special Intention"),
@@ -86,6 +89,7 @@ defmodule DonationWeb.ReportController do
     from_date = get_in(params, ["search", "from_date"])
     new_date = Date.from_iso8601!(from_date)
     mass_offerings = Admins.find_mass_offering_dates(new_date)
+                     |> Admins.reject_verified_is_false()
     conn
     |> put_resp_content_type("text/xlsx")
     |> put_resp_header("content-disposition", "attachment; filename=mass_intention.xlsx;")
